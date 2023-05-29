7fcdcdbec313b1dd96a9f96ecb7919bd618b61bc
Dakkshesh <dakkshesh5@gmail.com>
2023-05-29 14:35:46 +0530

    Bitsy: main: Mark constant variables as static
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -24,43 +24,43 @@
 //define 12 servos for 4 legs
 Servo servo[4][3];
 //define servos' ports
-const int servo_pin[4][3] = { {2, 3, 4}, {5, 6, 7}, {8, 9, 10}, {11, 12, 13} };
+static const int servo_pin[4][3] = { {2, 3, 4}, {5, 6, 7}, {8, 9, 10}, {11, 12, 13} };
 /* Size of the robot ---------------------------------------------------------*/
-const float length_a = 55;
-const float length_b = 77.5;
-const float length_c = 27.5;
-const float length_side = 71;
-const float z_absolute = -28;
+static const float length_a = 55;
+static const float length_b = 77.5;
+static const float length_c = 27.5;
+static const float length_side = 71;
+static const float z_absolute = -28;
 /* Constants for movement ----------------------------------------------------*/
-const float z_default = -50, z_up = -30, z_boot = z_absolute;
-const float x_default = 62, x_offset = 0;
-const float y_start = 0, y_step = 40;
+static const float z_default = -50, z_up = -30, z_boot = z_absolute;
+static const float x_default = 62, x_offset = 0;
+static const float y_start = 0, y_step = 40;
 /* variables for movement ----------------------------------------------------*/
 volatile float site_now[4][3];    //real-time coordinates of the end of each leg
 volatile float site_expect[4][3]; //expected coordinates of the end of each leg
 float temp_speed[4][3];   //each axis' speed, needs to be recalculated before each movement
 float move_speed;     //movement speed
-const float speed_multiple = 1; //movement speed multiple
-const float spot_turn_speed = 4;
-const float leg_move_speed = 8;
-const float body_move_speed = 3;
-const float stand_seat_speed = 1;
+static const float speed_multiple = 1; //movement speed multiple
+static const float spot_turn_speed = 4;
+static const float leg_move_speed = 8;
+static const float body_move_speed = 3;
+static const float stand_seat_speed = 1;
 volatile int rest_counter;      //+1/0.02s, for automatic rest
 //functions' parameter
-const float KEEP = 255;
+static const float KEEP = 255;
 //define PI for calculation
-const float pi = 3.1415926;
+static const float pi = 3.1415926;
 /* Constants for turn --------------------------------------------------------*/
 //temp length
-const float temp_a = sqrt(pow(2 * x_default + length_side, 2) + pow(y_step, 2));
-const float temp_b = 2 * (y_start + y_step) + length_side;
-const float temp_c = sqrt(pow(2 * x_default + length_side, 2) + pow(2 * y_start + y_step + length_side, 2));
-const float temp_alpha = acos((pow(temp_a, 2) + pow(temp_b, 2) - pow(temp_c, 2)) / 2 / temp_a / temp_b);
+static const float temp_a = sqrt(pow(2 * x_default + length_side, 2) + pow(y_step, 2));
+static const float temp_b = 2 * (y_start + y_step) + length_side;
+static const float temp_c = sqrt(pow(2 * x_default + length_side, 2) + pow(2 * y_start + y_step + length_side, 2));
+static const float temp_alpha = acos((pow(temp_a, 2) + pow(temp_b, 2) - pow(temp_c, 2)) / 2 / temp_a / temp_b);
 //site for turn
-const float turn_x1 = (temp_a - length_side) / 2;
-const float turn_y1 = y_start + y_step / 2;
-const float turn_x0 = turn_x1 - temp_b * cos(temp_alpha);
-const float turn_y0 = temp_b * sin(temp_alpha) - turn_y1 - length_side;
+static const float turn_x1 = (temp_a - length_side) / 2;
+static const float turn_y1 = y_start + y_step / 2;
+static const float turn_x0 = turn_x1 - temp_b * cos(temp_alpha);
+static const float turn_y0 = temp_b * sin(temp_alpha) - turn_y1 - length_side;
 
 /* Debug */
 #define DEBUG_BUILD 0
